11eedc5ab65aab78ca2e328fa62e6a698d01c932
c <freqnlabs@gmail.com>
2014-09-13 11:32:04 -0400

    Add update & delete CRUD operations to species

--- app/controllers/species_controller.rb
+++ app/controllers/species_controller.rb
@@ -3,4 +3,40 @@ class SpeciesController < ApplicationController
     @species = Species.all
     render 'species/index'
   end
+
+  def new
+    @species = Species.new
+    render 'species/new'
+  end
+
+  def create
+    @species = Species.create(name: params[:name])
+    if @species.save
+      render 'species/success'
+    else
+      render 'species/new'
+    end 
+  end
+
+  def edit
+    @species = Species.find(params[:id])
+    render 'species/edit'
+  end
+
+  def update
+    @species = Species.find(params[:id])
+    if @species.update(name: params[:name])
+      render 'species/success'
+    else
+      render 'species/edit'
+    end
+  end
+
+  def destroy
+    @species = Species.find(params[:id])
+    @species.destroy
+    render 'species/destroy'
+  end
+
+
 end
